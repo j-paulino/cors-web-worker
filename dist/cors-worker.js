9d717378de9e012326a258a7ea51d6f2b7e9763b
Joel Paulino <jpaulino@yoicorp.com>
2016-05-22 18:32:25 -0700

    More clean up

--- dist/cors-worker.js
+++ dist/cors-worker.js
@@ -1,12 +1,10 @@
 (function () {
     "use strict";
-    // Creating a promise
-    //TODO; ADD support more VERBS
+    //TODO; ADD more support for more VERBS
     var http = {
         get: request('get')
     };
 
-    // Return the promise
     window.http = http;
 
     function request(verb) {
